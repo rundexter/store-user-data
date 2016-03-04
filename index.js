dac1cf0d6e26a35f11cf77183a68d7791653dd4e
Mike Friedman <friedo@friedo.com>
2016-03-04 15:29:03 -0500

    initial

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var _ = require( 'lodash' );
+
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +8,12 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var keys = step.input( 'key' ).toArray();
+        var vals = step.input( 'value' ).toArray();
+
+        _.zipWith( keys, vals, function( k, v ) { return { key: k, value: v } } ).forEach( function( item ) {
+            this.storage.setUser( item.key, item.value );
+        }.bind( this ) );
+        this.complete( { success: true } );
     }
 };
